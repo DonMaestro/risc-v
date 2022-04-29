ddef92676f5bbd373653341abd3b61bdeb72f53e
DonMaestro <vitalik16200@gmail.com>
2022-04-29 14:29:08 +0300

    fix(src/freelist.v): modified file

--- src/freelist.v
+++ src/freelist.v
@@ -1,4 +1,4 @@
-module freelist #(parameter WIDTH = 5, STNUM = 0)
+module freelist #(parameter WIDTH = 5, SIZE = $pow(2, WIDTH), STNUM = 0)
                 (output [WIDTH-1:0] o_data,
                  input  [WIDTH-1:0] i_data,
                  input              i_re,
@@ -8,22 +8,30 @@ module freelist #(parameter WIDTH = 5, STNUM = 0)
 integer i;
 
 reg [WIDTH-1:0] head, tail;
-reg [WIDTH-1:0] data[0:(2^WIDTH)-2]; //without prd = 0x00
+reg [WIDTH-1:0] data[0:SIZE-1]; 
 
 always @(posedge i_clk, negedge i_rst_n)
 begin
 	if (!i_rst_n) begin
 		head <= { WIDTH{1'b0} };
 		tail <= { WIDTH{1'b0} };
-		for (i = 0; i < (2^WIDTH)-1; i = i + 1) begin
+		for (i = 0; i < SIZE; i = i + 1) begin
 			data[i] = i + STNUM;
 		end
 	end else begin
-		if (i_re)
-			head <= head + 1;
+		if (i_re) begin
+			if (head >= SIZE - 1)
+				head <= { WIDTH{1'b0} };
+			else
+				head <= head + 1;
+		end
 
 		if (i_we) begin
-			tail <= tail + 1;
+			if (tail >= SIZE - 1)
+				tail <= { WIDTH{1'b0} };
+			else
+				tail <= tail + 1;
+
 			data[tail] <= i_data;
 		end
 	end
