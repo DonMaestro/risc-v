1e7dbee01530b0995e9d6f56503041b7571d8fd4
DonMaestro <vitalik16200@gmail.com>
2022-05-01 20:12:47 +0300

    fix(src/br.v): change logic

--- src/br.v
+++ src/br.v
@@ -1,22 +1,20 @@
-module br #(parameter WIDTH_BRM = 4)
-          (output reg [WIDTH_BRM-1:0] o_killbr,
-           input wire [3:0]           i_control,
-           input wire [WIDTH_BRM-1:0] i_brmask,
-           input wire [31:0]          i_op1, i_op2);
+module br(output reg        o_result,
+          input wire [2:0]  i_control,
+          input wire [31:0] i_op1, i_op2);
 
-localparam BEQ = 4'h0, BNE = 4'h1;
-localparam BLT = 4'h4, BGE = 4'h5, BLTU = 4'h6, BGEU = 4'h7;
+localparam BEQ = 3'h0, BNE = 3'h1;
+localparam BLT = 3'h4, BGE = 3'h5, BLTU = 3'h6, BGEU = 3'h7;
 
 always @(*)
 begin
 	case(i_control)
-		BEQ : o_killbr = (i_op1 == i_op2) ? i_brmask + 1 : i_brmask + 2;
-		BNE : o_killbr = (i_op1 != i_op2) ? i_brmask + 1 : i_brmask + 2; 
-		BLT : o_killbr = (i_op1 <  i_op2) ? i_brmask + 1 : i_brmask + 2; 
-		BGE : o_killbr = (i_op1 >= i_op2) ? i_brmask + 1 : i_brmask + 2; 
-		BLTU: o_killbr = (i_op1 <  i_op2) ? i_brmask + 1 : i_brmask + 2; 
-		BGEU: o_killbr = (i_op1 >= i_op2) ? i_brmask + 1 : i_brmask + 2; 
-		default	: o_killbr = { WIDTH_BRM{1'bX} };
+		BEQ : o_result = (i_op1 == i_op2) ? 1'b1 : 1'b0;
+		BNE : o_result = (i_op1 != i_op2) ? 1'b1 : 1'b0; 
+		BLT : o_result = (i_op1 <  i_op2) ? 1'b1 : 1'b0; 
+		BGE : o_result = (i_op1 >= i_op2) ? 1'b1 : 1'b0; 
+		BLTU: o_result = (i_op1 <  i_op2) ? 1'b1 : 1'b0; 
+		BGEU: o_result = (i_op1 >= i_op2) ? 1'b1 : 1'b0; 
+		default	: o_result = 1'bX;
 	endcase
 end
 
