227ce408fb868fc4f7cbdf0a1191fae49cecaac4
DonMaestro <vitalik16200@gmail.com>
2022-05-13 22:04:12 +0300

    fix(src/rob.v): modify file

--- src/rob.v
+++ src/rob.v
@@ -9,12 +9,13 @@ module rob #(parameter WIDTH_BANK = 3, WIDTH_REG = 7, WIDTH_BRM = 4,
             input  [31:0]             i_dis_pc,
             input  [4*WIDTH-1:0]      i_dis_data4x,
             input                     i_dis_we,
-            input  [4*WIDTH_BANK-1:0] i_rst4x_valtg,
+            input  [WIDTH_BRM:0]      i_kill,
             input  [4*WIDTH_BANK-1:0] i_rst4x_busytg,
             input                     i_rst_n,
             input                     i_clk);
 
 localparam NBANK = 4;
+localparam WIDTH_DT = WIDTH - 2 - WIDTH_BRM;
 localparam integer SIZE = $pow(2, WIDTH_BANK);
 
 wire [WIDTH-1:0] new_data[0:NBANK-1];
@@ -56,18 +57,21 @@ generate
 		assign com_val[i] = commit[i][WIDTH-1];
 		assign com_prd[i] = commit[i][WIDTH_BRM+WIDTH_REG-1:WIDTH_BRM];
 
-		bank m_inst_b(.o_data(commit[i]),
+		bank m_inst_b(.o_pkg(commit[i]),
+		              .i_pkg(new_data[i]),
 		              .i_head(head),
 		              .i_tail(tail),
-		              .i_data(new_data[i]),
+	                      .i_killMask(i_kill),
 		              .i_re(re),
 		              .i_we(we),
+		              .i_rst_busy(),
 		              .i_rst_n(i_rst_n),
 		              .i_clk(i_clk));
-		defparam m_inst_b.WIDTH = WIDTH;
-		defparam m_inst_b.SIZE = SIZE;
+		defparam m_inst_b.SIZE      = SIZE;
+		defparam m_inst_b.WIDTH_DT  = WIDTH_DT;
 		defparam m_inst_b.WIDTH_REG = WIDTH_REG;
 		defparam m_inst_b.WIDTH_BRM = WIDTH_BRM;
+		defparam m_inst_b.WIDTH     = WIDTH;
 	end
 endgenerate
 
@@ -78,43 +82,125 @@ defparam m_encoder.SIZE = SIZE;
 endmodule
 
 
-module bank #(parameter WIDTH = 13, SIZE = 20, WIDTH_REG = 7, WIDTH_BRM = 4)
-            (output [WIDTH-1:0]   o_data,
+/**
+ * bank
+ */
+module bank #(parameter SIZE = 32, WIDTH_DT = 39, WIDTH_REG = 7, WIDTH_BRM = 4,
+                        WIDTH = 2 + WIDTH_DT + WIDTH_BRM)
+            (output [WIDTH-1:0]   o_pkg,
+             input  [WIDTH-1:0]   i_pkg,
              input  [SIZE-1:0]    i_head, i_tail,
-             input  [WIDTH-1:0]   i_data,
-             input  [WIDTH_BRM:0] i_killmask, // { enkill, brmask }
-             input                i_rst_busy,
-             //input  i_rst_val, i_rst_busy, i_set_exc,
+             input  [WIDTH_BRM:0] i_killMask, // { enkill, brmask }
+             input  [SIZE-1:0]    i_rst_busy,
              input  i_re, i_we, i_rst_n, i_clk);
 
-localparam WIDTH_DT = WIDTH - 1 - WIDTH_BRM;
-wire [WIDTH-1:0] data[0:SIZE-1];
-wire [SIZE-1:0]  clear, en_busy, rst_busy;
+integer j;
+
+wire [WIDTH-1:0]      pkg[0:SIZE-1];
+reg  [WIDTH-1:0]      pkg_head;
+
+wire                 killEn;
+wire [WIDTH_BRM-1:0] killMask;
 
 // output
-
+assign o_pkg = pkg_head;
 /**
  * kill logic
  */
-
-
-/**
- * clean busy bit
- */
-assign rst_busy = 1 << i_rst_busy;
+assign killEn   = i_killMask[WIDTH_BRM];
+assign killMask = i_killMask[WIDTH_BRM-1:0];
 
 generate
 	genvar i;
 	for (i = 0; i < SIZE; i = i + 1) begin
-		slot m_slot(.o_q    (brmask[i]),
-		            .i_d    (new_brmask),
-		            .i_en   (i_head[i]),
-		            .i_rst_n(i_rst_n),
-		            .i_clk  (i_clk));
+		bankSlot m_slot(.o_pkg(pkg[i]),
+		                .i_pkg(i_pkg),
+		                .i_killMask(killMask),
+		                .i_killEn(killEn),
+		                .i_re(i_head[i]),
+		                .i_we(i_tail[i]),
+		                .i_rst_busy(i_rst_busy[i]),
+		                .i_rst_n(i_rst_n),
+		                .i_clk (i_clk));
+		defparam m_slot.WIDTH_DT  = WIDTH_DT;
 		defparam m_slot.WIDTH_REG = WIDTH_REG;
 		defparam m_slot.WIDTH_BRM = WIDTH_BRM;
+		defparam m_slot.WIDTH     = WIDTH;
 	end
 endgenerate
+
+always @(*)
+begin
+	pkg_head = { WIDTH{1'b0} };
+	for (j = 0; j < SIZE; j = j + 1) begin
+		pkg_head |= pkg[j];
+	end
+end
+
+endmodule
+
+
+/**
+ * slot
+ */
+module bankSlot #(parameter WIDTH_DT = 39, WIDTH_REG = 7, WIDTH_BRM = 4,
+                        WIDTH = 2 + WIDTH_DT + WIDTH_BRM)
+            (output [WIDTH-1:0]     o_pkg, 
+             input  [WIDTH-1:0]     i_pkg, 
+             input  [WIDTH_BRM-1:0] i_killMask,
+             input                  i_killEn,
+             input                  i_re, i_we,
+             input                  i_rst_busy,
+             input                  i_rst_n, i_clk);
+
+wire [WIDTH-1:0]  pkg;
+
+wire                 val,    val_new, val_rst;
+wire                 busy,   busy_new;
+wire [WIDTH_DT-1:0]  data,   data_new;
+wire [WIDTH_BRM-1:0] brmask, brmask_new;
+
+// output
+assign o_pkg = i_re ? pkg : { WIDTH{1'b0} };
+
+
+assign pkg = { val, busy, data, brmask };
+
+assign val_new = i_pkg[WIDTH_DT+WIDTH_BRM+1];
+assign val_rst = brkill(i_killEn, i_killMask, brmask, brmask);
+
+assign busy_new = i_pkg[WIDTH_DT+WIDTH_BRM];
+
+assign data_new   = i_pkg[WIDTH_DT+WIDTH_BRM-1:WIDTH_BRM];
+assign brmask_new = i_pkg[WIDTH_BRM-1:0];
+
+sreg #(1) r_val(.o_q    (val),
+                .i_d    (val_new),
+                .i_en   (i_we),
+                .i_srsh (val_rst),
+                .i_rst_n(i_rst_n),
+                .i_clk  (i_clk));
+
+sreg #(1) r_busy(.o_q    (busy),
+                 .i_d    (busy_new),
+                 .i_en   (i_we),
+                 .i_srsh (i_rst_busy),
+                 .i_rst_n(i_rst_n),
+                 .i_clk  (i_clk));
+
+register r_data(.o_q    (data),
+                .i_d    (data_new),
+                .i_en   (i_we),
+                .i_rst_n(i_rst_n),
+                .i_clk  (i_clk));
+defparam r_data.WIDTH = WIDTH_DT;
+
+register r_brmask(.o_q    (brmask),
+                  .i_d    (brmask_new),
+                  .i_en   (i_we),
+                  .i_rst_n(i_rst_n),
+                  .i_clk  (i_clk));
+defparam r_brmask.WIDTH = WIDTH_BRM;
 
 // fun
 function brkill(input                 kill_en,
@@ -136,58 +222,23 @@ endfunction
 
 endmodule
 
+// register with synchronous reset
+module sreg #(parameter WIDTH=32)
+            (output reg [WIDTH-1:0] o_q,
+             input wire [WIDTH-1:0] i_d,
+             input i_en, i_srsh, i_rst_n, i_clk);
 
-// slot
-module slot #(parameter  SIZE = 8, WIDTH_REG = 7, WIDTH_BRM = 4)
-            (output [WIDTH_DT-1:0] i_data, 
-             input                 i_rst_n, i_clk);
-
-wire new_val;
-wire [WIDTH_DT-1:0] new_data;
-wire [WIDTH_BRM-1:0] new_brmask;
-
-wire [WIDTH_BRM-1:0] killmask;
-wire                 enkill;
-
-wire [WIDTH_BRM-1:0] brmask;
-
-
-assign enkill = i_killmask[WIDTH_BRM];
-assign killmask = i_killmask[WIDTH_BRM-1:0];
-
-assign new_val = i_data[WIDTH_DT-1];
-assign new_data = i_data[WIDTH_DT-2:WIDTH_BRM];
-assign new_brmask = i_data[WIDTH_BRM-1:0];
-
-assign clear = brkill(enkill, killmask, brmask[i], brmask[i]);
-
-assign en_busy = i_tail | rst_busy;
-
-register #(1) r_val(.o_q    (val),
-                    .i_d    (new_val),
-                    .i_en   (i_tail | clear),
-                    .i_rst_n(i_rst_n),
-                    .i_clk  (i_clk));
-
-register #(1) r_busy(.o_q    (busy),
-                     .i_d    (1'b1),
-                     .i_en   (i_head),
-                     .i_rst_n(i_rst_n),
-                     .i_clk  (i_clk));
-
-register r_data(.o_q    (data),
-                .i_d    (new_data),
-                .i_en   (i_tail),
-                .i_rst_n(i_rst_n),
-                .i_clk  (i_clk));
-defparam r_data.WIDTH = WIDTH_DT;
-
-register r_brmask(.o_q    (brmask),
-                  .i_d    (new_brmask),
-                  .i_en   (i_head),
-                  .i_rst_n(i_rst_n),
-                  .i_clk  (i_clk));
-defparam r_brmask.WIDTH = WIDTH_BRM;
+always @(posedge i_clk, negedge i_rst_n)
+begin
+	if (!i_rst_n)
+		o_q <= { WIDTH{1'b0} };
+	else if (i_srsh) begin
+		o_q <= { WIDTH{1'b0} };
+	end else begin
+		if (i_en)
+			o_q <= i_d;
+	end
+end
 
 endmodule
 
