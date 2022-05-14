69aa60d6f51e268cefe9f8fb71689626d66f1eaf
DonMaestro <vitalik16200@gmail.com>
2022-05-14 14:21:12 +0300

    fix(Test/tb_rob.v): fix interface of module

--- Test/tb_rob.v
+++ Test/tb_rob.v
@@ -7,27 +7,32 @@
 module tb_rob;
 
 integer i;
+localparam WIDTH_BANK = 3;
 localparam WIDTH_REG = 7;
 localparam WIDTH_BRM = 4;
-localparam WIDTH_BANK = 3;
 localparam WIDTH = 2 + 7 + 32 + WIDTH_REG + WIDTH_BRM;
+
+localparam WIDTH_BRST = 1 + WIDTH_BANK + 2; // 4 banks
+
+reg rst_n, clk;
+
+reg                     we;
+reg  [31:0]             PC;
+wire [4*WIDTH-1:0]      data4x;
+wire [4*WIDTH_BRST-1:0] rst4x_busy;
 
 wire [WIDTH_BANK-1:0]  o_tag;
 wire [4*WIDTH_REG-1:0] o_com_prd4x;
 wire                   o_com_en;
-reg             rst_n, clk;
  
-reg [31:0]            PC;
-wire [4*WIDTH-1:0]  data4x;
-reg                 we;
-
-reg [WIDTH-1:0]     data[0:3];
-reg                 val[0:3];
-reg                 busy[0:3];
-reg [7-1:0]         uop[0:3];
-reg [WIDTH_BRM-1:0] brm[0:3];
-reg [WIDTH_REG-1:0] prd[0:3];
-reg [32-1:0]        imm[0:3];
+reg [WIDTH-1:0]      data[0:3];
+reg                  val[0:3];
+reg                  busy[0:3];
+reg [7-1:0]          uop[0:3];
+reg [WIDTH_BRM-1:0]  brm[0:3];
+reg [WIDTH_REG-1:0]  prd[0:3];
+reg [32-1:0]         imm[0:3];
+reg [WIDTH_BRST-1:0] rst_busy[0:3]; // EN[1] + [WIDTH_BANK] + SET_BANK[2]
 
 always @(*)
 begin
@@ -35,6 +40,7 @@ begin
 		data[i] = { val[i], busy[i], uop[i], imm[i], prd[i], brm[i]};
 	end
 end
+
 assign data4x = { data[3], data[2], data[1], data[0] };
 
 rob m_rob(.o_dis_tag(o_tag),
@@ -44,31 +50,26 @@ rob m_rob(.o_dis_tag(o_tag),
           .i_dis_data4x(data4x),
           .i_dis_we(we),
           .i_kill({ (WIDTH_BRM+1){1'b0} }),
-          .i_rst4x_busytg(),
+          .i_rst_busy0(rst_busy[0]),
+          .i_rst_busy1(rst_busy[1]),
+          .i_rst_busy2(rst_busy[2]),
+          .i_rst_busy3(rst_busy[3]),
           .i_rst_n(rst_n),
           .i_clk(clk));
-defparam m_rob.WIDTH_REG = WIDTH_REG;
-defparam m_rob.WIDTH_BRM = WIDTH_BRM;
 defparam m_rob.WIDTH_BANK = WIDTH_BANK;
+defparam m_rob.WIDTH_REG  = WIDTH_REG;
+defparam m_rob.WIDTH_BRM  = WIDTH_BRM;
 
 initial
 begin
 	rst_n = 1'b0;
 	rst_n <= #1 1'b1;
 
-	$display("gg %d", m_rob.we);
+	we = 1'b1;
 
-	PC = 32'b1;
-
-	val[0] = 1'b1;
-	val[1] = 1'b1;
-	val[2] = 1'b1;
-	val[3] = 1'b1;
-
-	busy[0] = 1'b1;
-	busy[1] = 1'b1;
-	busy[2] = 1'b1;
-	busy[3] = 1'b1;
+	PC = 32'b0;
+	val[0] = 1'b1; val[1] = 1'b1; val[2] = 1'b1; val[3] = 1'b1;
+	busy[0] = 1'b1; busy[1] = 1'b1; busy[2] = 1'b1; busy[3] = 1'b1;
 
 	uop[0] = 7'h4f;
 	uop[1] = 7'h40;
@@ -90,12 +91,31 @@ begin
 	imm[2] = 32'hff00;
 	imm[3] = 32'hff;
 
-	we = 1'b1;
+end
+
+initial
+begin
+	rst_busy[0] = { 1'b0, 3'h0, 2'b00 };
+	rst_busy[1] = { 1'b0, 3'h0, 2'b01 };
+	rst_busy[2] = { 1'b0, 3'h0, 2'b10 };
+	rst_busy[3] = { 1'b0, 3'h0, 2'b11 };
+
+	#40
+	rst_busy[0] = { 1'b1, 3'h0, 2'b00 };
+	rst_busy[1] = { 1'b1, 3'h0, 2'b01 };
+	rst_busy[2] = { 1'b1, 3'h0, 2'b10 };
+	rst_busy[3] = { 1'b1, 3'h0, 2'b11 };
+
+	#40
+	rst_busy[0] = { 1'b1, 3'h1, 2'b00 };
+	rst_busy[1] = { 1'b1, 3'h1, 2'b01 };
+	rst_busy[2] = { 1'b1, 3'h1, 2'b10 };
+	rst_busy[3] = { 1'b1, 3'h1, 2'b11 };
 end
 
 always @(posedge clk)
 begin
-	PC <= PC + 4;
+	PC <= PC + 16; // 4 insturtions(each 4 byte)
 	brm[0] <= brm[0] + 4'h1;
 	brm[1] <= brm[1] + 4'h1;
 	brm[2] <= brm[2] + 4'h1;
