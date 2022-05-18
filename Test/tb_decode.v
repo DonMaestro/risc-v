a0f115371aafd2cccff3b20e34874929db0de796
DonMaestro <vitalik16200@gmail.com>
2022-05-18 10:58:37 +0300

    feat: decode module

--- Test/tb_decode.v
+++ Test/tb_decode.v
@@ -1,49 +1,94 @@
-`timescale 1 ns / 10 ps
+`timescale 1 ns / 1 ns
+`include "src/register.v"
 `include "src/decode.v"
 `include "src/InstrDecoder.v"
 `include "src/signExtend.v"
 
 module tb_decode;
 
-reg clk;
+localparam WIDTH_BRM = 3;
+reg rst_n, clk;
 
 reg [31:0] Instr;
 reg        Imask;
+reg        en;
 
 wire [14:0] regs;
 wire [31:0] dimm;
 wire [9:0] func;
 wire [4:0] ctrl;
 
+wire [WIDTH_BRM-1:0] o_brmask, brmask;
+wire                 o_en_bm,  en_bm;
+
 wire [4:0] drd, drs1, drs2;
 
 assign { drd, drs2, drs1 } = regs;
 
-decode mod_decode(.o_regs (regs),
-                  .o_func (func),
-                  .o_ctrl (ctrl),
-                  .o_imm  (dimm),
-                  .i_instr(Instr),
-                  .i_imask(Imask));
+register #(1) r_en_j(en_bm, 1'b1, o_en_bm, rst_n, clk);
+register      r_brmask(brmask, 1'b1, o_brmask, rst_n, clk);
+defparam r_brmask.WIDTH = WIDTH_BRM;
+
+decode m_decode(.o_regs  (regs),
+                .o_func  (func),
+                .o_ctrl  (ctrl),
+                .o_imm   (dimm),
+                .o_en_j  (o_en_bm),
+                .o_brmask(o_brmask),
+                .i_en_j  (en_bm),
+                .i_brmask(brmask),
+                .i_en    (en),
+                .i_instr (Instr),
+                .i_imask (Imask));
+defparam m_decode.WIDTH_BRM = WIDTH_BRM;
 
 initial
 begin
+	en    = 1;
+	Imask = 1;
+	Instr = 32'h00002137;
+	$display("lui     sp,0x2");
+
+	#40
+	Imask = 1;
+	Instr = 32'h044000ef;
+	$display("jal     ra,48 <main>");
+
+	#40
 	Imask = 1;
 	Instr = 32'hfe010113;
-
-	#10
+	$display("addi    sp,sp,-32");
+	
+	#40
 	Imask = 1;
-	Instr = 32'h00112e23;
-
-	#10
+	Instr = 32'h00812e23;
+	$display("sw      s0,28(sp)");
+	
+	#40
+	en    = 1;
+	Imask = 0;
+	Instr = 32'h00812e23;
+	$display("sw      s0,28(sp)");
+	
+	#40
+	en    = 0;
 	Imask = 1;
-	Instr = 32'h01c0006f;
+	Instr = 32'h00812e23;
+	$display("sw      s0,28(sp)");
+	
+	#40
+	en    = 0;
+	Imask = 0;
+	Instr = 32'h00812e23;
+	$display("sw      s0,28(sp)");
 	
 end
 	  
 initial
 begin
+	rst_n = 0;
 	clk = 0;
+	rst_n <= #10 1;
 	forever #20 clk = ~clk;
 end
 
--- src/decode.v
+++ src/decode.v
@@ -1,29 +1,46 @@
-module decode(output reg [14:0] o_regs,
-              output reg [ 9:0] o_func,
-              output reg [ 4:0] o_ctrl,
-              output     [31:0] o_imm,
-              input wire [31:0] i_instr,
-              input wire        i_imask);
+module decode #(parameter WIDTH_BRM = 6)
+              (output reg [14:0]      o_regs,
+               output reg [ 9:0]      o_func,
+               output reg [ 4:0]      o_ctrl,
+               output     [31:0]      o_imm,
+               output reg             o_en_j,
+               output [WIDTH_BRM-1:0] o_brmask,
+               input wire             i_en_j,
+               input [WIDTH_BRM-1:0]  i_brmask,
+               input wire             i_en,
+               input wire [31:0]      i_instr,
+               input wire             i_imask);
 
-localparam [2:0] RT = 3'd0,
-                 IT = 3'd1,
-                 ST = 3'd2,
-                 BT = 3'd3,
-                 UT = 3'd4,
-                 JT = 3'd5;
+`include "src/IType.v"
 
 wire [2:0] ImmSrc;
 
 reg [4:0] drd, drs1, drs2;
 reg [2:0] funct3;
 reg [6:0] funct7;
+wire [1:0] queue;
 
-InstrDecoder m_decod(.o_ImmSrc(ImmSrc),
-                     .i_Op(i_instr[6:0]));
+assign o_brmask = i_en_j ? i_brmask + 1 : i_brmask;
 
-signExtend mod_signEntend(.o_data(o_imm),
-                          .i_en(ImmSrc),
-                          .i_data(i_instr[31:7]));
+always @(i_instr[6:0])
+begin
+	case (i_instr[6:0])
+		7'b1100011: o_en_j = 1;
+		7'b1101111: o_en_j = 1;
+		7'b1100111: o_en_j = 1;
+		default:    o_en_j = 0;
+	endcase
+end
+
+InstrDecoder m_decode(.o_ImmSrc(ImmSrc),
+                      .i_Op(i_instr[6:0]));
+
+signExtend m_signEntend(.o_data(o_imm),
+                        .i_en(ImmSrc),
+                        .i_data(i_instr[31:7]));
+
+type_queue m_type_queue(.o_type(queue),
+                        .i_uop(i_instr[6:0]));
 
 always @(*)
 begin
@@ -34,7 +51,7 @@ begin
 	funct7 = i_instr[31:25];
 	funct3 = i_instr[14:12];
 
-	o_ctrl = 5'b11;
+	o_ctrl = { queue, i_en };
 
 	case(ImmSrc)
 		RT:
@@ -70,6 +87,34 @@ begin
 			o_regs = { drd, 5'b0, 5'b0 };
 			o_func = { 7'b0, 3'b0 };
 		end
+		default:
+		begin
+			o_regs = { 5'b0, 5'b0, 5'b0 };
+			o_func = { 7'b0, 3'b0 };
+		end
+	endcase
+
+	if (!i_imask) begin
+		o_regs = { 5'b0, 5'b0, 5'b0 };
+		o_func = { 7'b0, 3'b0 };
+	end
+end
+
+endmodule
+
+module type_queue(output reg [1:0] o_type,
+                  input      [6:0] i_uop);
+
+localparam [1:0] MEMQ = 2'b01, ALUQ = 2'b10;
+
+always @(*)
+begin
+	casez (i_uop)
+	7'b0?00011: o_type = MEMQ;
+//	7'b1100011: o_type =
+//	7'b1101111: o_type =
+//	7'b1100111: o_type =
+	default:    o_type = ALUQ;
 	endcase
 end
 
