4ba4a939e14c96f986997253aa914fae540239ab
DonMaestro <vitalik16200@gmail.com>
2022-05-28 09:32:06 +0300

    feat(src/bypass.v): add more bypasses

--- src/bypass.v
+++ src/bypass.v
@@ -5,7 +5,8 @@ module bypass #(parameter WIDTH_REG = 5)
               (output [2*32-1:0]         o_data0, o_data1, o_data2, o_data3,
                input  [2*WIDTH_REG-1:0]  i_irs0, i_irs1, i_irs2, i_irs3,
                input  [2*32-1:0]         i_regFile0, i_regFile1, i_regFile2, i_regFile3,
-               input  [32+WIDTH_REG:0]   i_bypass0, i_bypass1, i_bypass2, i_bypass3);
+               input  [32+WIDTH_REG:0]   i_bypass0, i_bypass1, i_bypass2, i_bypass3,
+               input  [32+WIDTH_REG:0]   i_bypass4, i_bypass5, i_bypass6);
 
 integer i, j;
 
@@ -13,9 +14,9 @@ wire [WIDTH_REG-1:0] rs[0:7];
 
 wire [32-1:0]        freg[0:7];
 
-wire                 val[0:3];
-wire [WIDTH_REG-1:0] brs[0:3];
-wire [32-1:0]        breg[0:3];
+wire                 val[0:6];
+wire [WIDTH_REG-1:0] brs[0:6];
+wire [32-1:0]        breg[0:6];
 
 reg [32-1:0] rg[0:7];
 
@@ -34,11 +35,15 @@ assign { val[1], brs[1], breg[1] } = i_bypass1;
 assign { val[2], brs[2], breg[2] } = i_bypass2;
 assign { val[3], brs[3], breg[3] } = i_bypass3;
 
+assign { val[4], brs[4], breg[4] } = i_bypass4;
+assign { val[5], brs[5], breg[5] } = i_bypass5;
+assign { val[6], brs[6], breg[6] } = i_bypass6;
+
 always @(*)
 begin
 	for (i = 0; i < 8; i = i + 1) begin
 		rg[i] = freg[i];
-		for (j = 0; j < 4; j = j + 1) begin
+		for (j = 0; j < 7; j = j + 1) begin
 			if (val[j] && rs[i] == brs[j])
  				rg[i] = breg[j];
 		end
