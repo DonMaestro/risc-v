79e39fff6790be42e01b4c4b4f482f65294c51dc
DonMaestro <vitalik16200@gmail.com>
2022-06-05 09:11:48 +0300

    fix(src/killf.v): automatic function

--- src/killf.v
+++ src/killf.v
@@ -1,6 +1,6 @@
 
-function killf(input [WIDTH_BRM-1:0]          mask,
-               input [$pow(2, WIDTH_BRM)-1:0] kmask);
+function automatic killf(input [WIDTH_BRM-1:0]          mask,
+                         input [$pow(2, WIDTH_BRM)-1:0] kmask);
 reg [$pow(2, WIDTH_BRM)-1:0] dmask;
 begin
 	// decoder
