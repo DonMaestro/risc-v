6a059057fd0e2afc293cb8dcf2588cc7b5d7ca42
DonMaestro <vitalik16200@gmail.com>
2022-07-08 09:17:07 +0300

    feat(src/AGU/AGU.v): minor update

--- src/AGU/AGU.v
+++ src/AGU/AGU.v
@@ -1,5 +1,5 @@
 module AGU #(parameter WIDTH_MEM = 4, WIDTH_BRM = 4, WIDTH_REG = 5,
-                       WIDTH = 1 + 7 + WIDTH_BRM + WIDTH_REG + 10 + 4*32)
+                       WIDTH = 2 + 7 + WIDTH_BRM + WIDTH_REG + 10 + 4*32)
            (output [32+WIDTH_REG:0] o_bypass, // { val, WIDTH_REG, data }
             // regFile
             output [31:0]          o_data,
@@ -18,26 +18,20 @@ module AGU #(parameter WIDTH_MEM = 4, WIDTH_BRM = 4, WIDTH_REG = 5,
 
 `include "src/killf.v"
 
-localparam SIZE = $pow(2, WIDTH_MEM-1);
+localparam SIZE = 2 ** WIDTH_MEM;
 localparam IT = 2'b01, ST = 2'b10, OT = 2'b00;
 
-//
-wire [31:0] pc;
-
 reg [1:0] FMT;
-
-reg [7:0] data[0:SIZE-1];
 
 wire [WIDTH-1:0] instr;
 
 wire [31:0] op1, op2, imm;
 wire [ 6:0] uop;
+wire [31:0] pc;
 wire [WIDTH_BRM-1:0] brmask;
 wire [ 9:0] func;
-wire        val;
+wire [ 1:0] val;
 wire [WIDTH_REG-1:0] rd; // result register
-
-wire [31:0]   data_r;
 
 wire [32-1:0] naddr, qaddr, laddr, saddr;
 wire [32-1:0] vaddr;
@@ -50,11 +44,6 @@ wire [WIDTH_MEM-1:0] paddrmatch;
 wire [31:0] dcache_data, sdq_data;
 
 reg valOut;
-
-initial
-begin
-	$readmemh("ram.dat", data);
-end
 
 // input Flip-Flop state
 register r_pipeI(instr, 1'b1, i_instr, i_rst_n, i_clk);
@@ -90,9 +79,9 @@ assign TLBms = 1'b0;
 
 //DCache
 assign dcache_data   = dcache_o_data;
-assign dcache_i_addr = paddr;
-assign dcache_i_data = op2;
 assign dcache_i_kill = ~match;
+register #(32) r_dc_addr(dcache_i_addr, 1'b1, paddr, i_rst_n, i_clk);
+register #(32) r_dc_data(dcache_i_data, 1'b1,   op2, i_rst_n, i_clk);
 
 register r_paddr(paddrmatch, ~TLBms, paddr, i_rst_n, i_clk);
 
@@ -116,14 +105,19 @@ LAQ m_LAQ(.o_match(match),
           .i_clk(i_clk));
 defparam m_LAQ.WIDTH = 5;
 
-SDQ m_SDQ(.o_data(sdq_data),
-          .i_data(),
-          .i_tag(),
+SDQ m_SDQ(.o_data (sdq_data),
           .i_raddr(),
-          .i_en(val),
+          .i_data (op2),
+          .i_rd   (rd),
+          .i_we   (val),
           .i_rst_n(i_rst_n),
-          .i_clk(i_clk));
+          .i_clk  (i_clk));
 defparam m_SAQ.WIDTH = 5;
+module ringbuf #(parameter WIDTH = 4, SIZE = 20)
+               (output wor [WIDTH-1:0] o_data,
+                output                 o_empty, o_overflow,
+                input      [WIDTH-1:0] i_data,
+                input      i_re, i_we, i_rst_n, i_clk);
 
 assign o_bypass = { valOut, rd, data_r };
 
