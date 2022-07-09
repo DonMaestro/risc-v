8673d871d1923a06b1a93938d6b4b594002ed67c
DonMaestro <vitalik16200@gmail.com>
2022-07-09 09:18:57 +0300

    fix(src/busytb.v): synthesis true

--- src/busytb.v
+++ src/busytb.v
@@ -14,7 +14,7 @@ module busytb #(parameter WIDTH = 5)
                input                i_clk);
 
 integer i;
-localparam SIZE = $pow(2, WIDTH);
+localparam SIZE = 2 ** WIDTH;
 
 reg busy[0:SIZE-1];
 wire [WIDTH-1:0] addr_set[0:3];
@@ -22,16 +22,16 @@ wire [WIDTH-1:0] addr_rst[0:3];
 
 generate
 	genvar j;
-	for (j = 0; j < 4; j = j + 1) begin
+	for (j = 0; j < 4; j = j + 1) begin: decode_addr
 		assign addr_set[j] = i_setAddr4x[(j+1)*WIDTH-1:j*WIDTH];
 		assign addr_rst[j] = i_rstAddr4x[(j+1)*WIDTH-1:j*WIDTH];
 	end
 
-	for (j = 0; j < 2; j = j + 1) begin
-		assign o_data1[j] = busy[i_addr1[(j+1)*WIDTH-1:j*WIDTH]];
-		assign o_data2[j] = busy[i_addr2[(j+1)*WIDTH-1:j*WIDTH]];
-		assign o_data3[j] = busy[i_addr3[(j+1)*WIDTH-1:j*WIDTH]];
-		assign o_data4[j] = busy[i_addr4[(j+1)*WIDTH-1:j*WIDTH]];
+	for (j = 0; j < 2; j = j + 1) begin: read_bit
+		assign o_data1[j] = i_addr1[(j+1)*WIDTH-1:j*WIDTH] ? busy[i_addr1[(j+1)*WIDTH-1:j*WIDTH]] : 1'b0;
+		assign o_data2[j] = i_addr2[(j+1)*WIDTH-1:j*WIDTH] ? busy[i_addr2[(j+1)*WIDTH-1:j*WIDTH]] : 1'b0;
+		assign o_data3[j] = i_addr3[(j+1)*WIDTH-1:j*WIDTH] ? busy[i_addr3[(j+1)*WIDTH-1:j*WIDTH]] : 1'b0;
+		assign o_data4[j] = i_addr4[(j+1)*WIDTH-1:j*WIDTH] ? busy[i_addr4[(j+1)*WIDTH-1:j*WIDTH]] : 1'b0;
 	end
 endgenerate
 
@@ -39,16 +39,15 @@ always @(posedge i_clk, negedge i_rst_n)
 begin
 	if (!i_rst_n)
 		for (i = 1; i < SIZE; i = i + 1)
-			busy[i] = 1'b1;
+			busy[i] <= 1'b1;
 	else begin
 
-		for (i = 0; i < WIDTH; i = i + 1) begin
+		for (i = 0; i < 4; i = i + 1) begin
 			busy[addr_set[i]] <= 1'b1;
 			busy[addr_rst[i]] <= 1'b0;
 		end
 
 	end
-	busy[0] <= 1'b0;
 end
 
 endmodule
