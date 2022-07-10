3fb37341ab8686df290b2602b2f04fb04712c7ac
DonMaestro <vitalik16200@gmail.com>
2022-07-10 09:06:13 +0300

    fix(src/ram.v)

--- src/ram.v
+++ src/ram.v
@@ -1,27 +1,21 @@
-module ram #(parameter ADDR_WIDTH = 32)
-           (output [31:0]           o_data,
+module ram #(parameter WIDTH_ADDR = 8,
+                       WIDTH_DATA = 8,
+                       SIZE = 2 ** WIDTH_DATA)
+           (output [WIDTH_DATA-1:0] o_data,
             input                   i_we,
-            input  [ADDR_WIDTH-1:0] i_addr,
-            input  [31:0]           i_data,
+            input  [WIDTH_ADDR-1:0] i_addr,
+            input  [WIDTH_DATA-1:0] i_data,
             input                   i_clk);
 
-reg [31:0] m_ram[0:2 ** ADDR_WIDTH - 1];
-
-initial
-begin
-	$readmemh("ram.dat", m_ram);
-end
+reg [WIDTH_DATA-1:0] m_ram[0:SIZE-1];
 
 always @(posedge i_clk)
 begin
 	if(i_we)
-	begin 
-		m_ram[i_addr >> 2] = i_data;
-		$writememh("Debug/ram_result.dat", m_ram);
-	end
+		m_ram[i_addr] <= i_data;
 end
 
-assign o_data = m_ram[i_addr >> 2];
+assign o_data = m_ram[i_addr];
 
 endmodule
 
