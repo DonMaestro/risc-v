def352194210cf47dffd0b944bfc7fa78274c892
DonMaestro <vitalik16200@gmail.com>
2022-07-14 10:02:28 +0300

    del: unused files and fix .gitignore

--- src/IType.v
+++
@@ -1,9 +0,0 @@
-
-localparam [2:0] NO = 3'd0,
-                 RT = 3'd1,
-                 IT = 3'd2,
-                 ST = 3'd3,
-                 BT = 3'd4,
-                 UT = 3'd5,
-                 JT = 3'd6;
-
